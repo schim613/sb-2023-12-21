71ed514cdd4707011b4bb005af2c56eef7fab658
schim613 <uximin@naver.com>
2023-12-21 14:24:51 +0900

    setting 2

---
+++ src/main/java/com/ll/sb231221/Sb231221Application.java
@@ -0,0 +1,12 @@
+package com.ll.sb231221;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb231221Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb231221Application.class, args);
+    }
+}
---
+++ src/test/java/com/ll/sb231221/Sb231221ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ll.sb231221;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class Sb231221ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
